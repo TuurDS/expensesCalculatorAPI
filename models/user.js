1752dcd0972a71e221a3ee9b0680ca9c7164c65c
Jitse Laceur Belon <jitse.laceurbelon@student.hogent.be>
2022-06-10 14:14:44 +0200

    finish demo sequelize
    Co-authored-by: TuurDS <TuurDS@users.noreply.github.com>

--- models/user.js
+++ models/user.js
@@ -3,17 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
+  class User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Posts}) {
       // define association here
+      this.hasMany(Posts, {foreignKey: 'userId', as: 'posts'})
+    }
+
+    toJSON() {
+      return {...this.get(), id: undefined}
     }
   }
-  user.init({
+  User.init({
     uuid: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
@@ -21,15 +26,23 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: { msg: 'User must have a name'},
+        notEmpty: { msg: 'name must not be empty'},
+      }
     }, 
     role:  {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: { msg: 'User must have a role'},
+        notEmpty: { msg: 'role must not be empty'},
+      }
     }
   }, {
     sequelize,
     tableName: 'user',
-    modelName: 'user',
+    modelName: 'User',
   });
-  return user;
+  return User;
 };
--- test.js
+++ test.js
@@ -1,15 +1,97 @@
-const { sequelize, user} = require('./models')
+const { sequelize, User, Posts} = require('./models')
 const express = require('express')
+const user = require('./models/user')
 
 const app = express()
 app.use(express.json())
 
 app.post('/user', async(req,res) => {
     const {name,role} = req.body
-    console.log(user);
     try {
-        const users = await user.create({name, role})
-        return res.json(users)
+        const user = await User.create({name, role})
+        return res.json(user)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.get('/user', async (req,res) => {
+    try {
+        const user = await User.findAll();
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(err)
+    }
+    }
+)
+
+app.get('/user/:uuid', async (req,res) => {
+    const uuid = req.params.uuid
+    try {
+        const user = await User.findOne({
+            where: {uuid},
+            include:"posts"
+        });
+
+        return res.json(user)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+    }
+)
+
+app.post('/posts', async(req,res) => {
+    const { userUuid, body} = req.body
+    try {
+        const user = await User.findOne({ where: {uuid: userUuid}})
+        const post = await Posts.create({body, userId: user.id})
+        return res.json(post)
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.get('/posts', async (req,res) => {
+    try {
+        const posts = await Posts.findAll({ include:['user']});
+
+        return res.json(posts)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+    }
+)
+
+app.delete('/user/:uuid', async (req,res) => {
+    const uuid = req.params.uuid
+    try {
+        const user = await User.findOne({where: {uuid}});
+        await user.destroy()
+        return res.json({message: 'User deleted!'})
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.put('/user/:uuid', async (req,res) => {
+    const uuid = req.params.uuid
+    const {name,role} = req.body
+    try {
+        const user = await User.findOne({where: {uuid}});
+        user.name = name
+        user.role = role
+
+        await user.save()
+
+        return res.json(user)
     } catch (err) {
         console.log(err)
         return res.status(500).json(err)
